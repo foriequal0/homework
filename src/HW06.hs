b8b0eafb8de2e080e28892c3c271c7b29a6454f4
SeongChan Lee <foriequal@gmail.com>
2015-01-13 05:31:46 +0900

    Add functions parseMarkets, loadData.

--- src/HW06.hs
+++ src/HW06.hs
@@ -4,16 +4,16 @@
 module HW06 where
 
 import           Data.Aeson
+import           Data.Char
 import           Data.Monoid
 import           GHC.Generics
-import           Data.Char
 
 import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.Text                  as T
 import qualified Data.Text.IO               as T
 
 ynToBool :: Value -> Value
-ynToBool (String s) = 
+ynToBool (String s) =
     case T.toLower(s) of
       "y" -> Bool True
       "n" -> Bool False
@@ -26,3 +26,30 @@ parseData :: B.ByteString -> Either String Value
 parseData = fmap ynToBool . eitherDecode
 
 
+data Market = Market { marketname :: T.Text,
+                       x          :: Float,
+                       y          :: Float,
+                       state      :: T.Text}
+            deriving (Show, Generic)
+
+instance FromJSON Market
+
+fromResult :: Result a -> Either String a
+fromResult (Error s) = Left s
+fromResult (Success a) = Right a
+
+parseMarkets :: B.ByteString -> Either String [Market]
+parseMarkets s = do
+  parsedValue <- parseData s
+  let result = fromJSON(parsedValue)
+  case result of
+    Error err -> fail err
+    Success a -> return a
+
+loadData :: IO [Market]
+loadData = do
+  filedata <- B.readFile "markets.json"
+  let eitherMarket = parseMarkets(filedata)
+  case eitherMarket of
+    Left err -> fail err
+    Right a -> return a
