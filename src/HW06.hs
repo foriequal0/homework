381334de1cb6a0ea40f5a82db320c4aca87bbb9a
SeongChan Lee <foriequal@gmail.com>
2015-01-13 04:23:41 +0900

    Added ynToBool.

---
+++ src/HW06.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module HW06 where
+
+import           Data.Aeson
+import           Data.Monoid
+import           GHC.Generics
+import           Data.Char
+
+import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.Text                  as T
+import qualified Data.Text.IO               as T
+
+ynToBool :: Value -> Value
+ynToBool (String s) = 
+    case T.toLower(s) of
+      "y" -> Bool True
+      "n" -> Bool False
+ynToBool (Object o) = Object $ fmap ynToBool o
+ynToBool (Array a) = Array $ fmap ynToBool a
+ynToBool v = v
+
+
